465f2e72a0caa22abbc8cabdbec83530894f5b83
Wetrovosk <ya.majorova@gmail.com>
2025-10-08 19:22:49 +0400

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,4 @@
+pet_type=input("Введите вид питомца: ")
+pet_name=input("Введите имя питомца: ")
+pet_age=input("Введите возраст питомца: ")
+print("Это "+pet_type+" по кличке "+pet_name+", возраст: "+pet_age)
